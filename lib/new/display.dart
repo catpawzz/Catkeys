0c90198d87e40b9391eb8c84a5d002402f0b7bf7
catpawz <cat@catpawz.net>
2025-02-15 07:39:39 +0100

    Enhance DisplayPage with user-specific information and improved UI elements; fetch and display user handle and profile picture dynamically.

--- lib/new/display.dart
+++ lib/new/display.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:misskey_dart/misskey_dart.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'drive.dart';
 import 'posts.dart';
 import 'profile.dart';
@@ -11,8 +14,14 @@ class DisplayPage extends StatefulWidget {
 }
 
 class _DisplayPageState extends State<DisplayPage> {
+  late Misskey client = Misskey(host: '', token: '');
+
   int _selectedIndex = 0;
   String appVersion = '-';
+  String url = '-';
+  String token = '-';
+  String userHandle = '-';
+  String profilePicture = '';
 
   static List<Widget> _widgetOptions = <Widget>[
     PostsPage(),
@@ -27,17 +36,29 @@ class _DisplayPageState extends State<DisplayPage> {
     });
   }
 
-  void fetchAppVersion() async {
+  void fetchInfo() async {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final res = await client.i.i();
     setState(() {
+      if (url.isNotEmpty && token.isNotEmpty) {
+        client = Misskey(
+          host: url,
+          token: token,
+        );
+      }
       appVersion = packageInfo.version;
+      url = prefs.getString('catkeys_url') ?? '';
+      token = prefs.getString('catkeys_token') ?? '';
+      profilePicture = res.avatarUrl.toString();
+      userHandle = res.username.toString();
     });
   }
 
   @override
   void initState() {
     super.initState();
-    // ...existing code...
+    fetchInfo();
   }
 
   @override
@@ -55,14 +76,14 @@ class _DisplayPageState extends State<DisplayPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Catkeys",
+              "Catkeys (NEW UI)",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 color: Theme.of(context).colorScheme.primary,
               ),
             ),
             Text(
-              'Ver. $appVersion | Connected to example.com', // Add your subtitle text here
+              'Ver. $appVersion | Connected to $url', // Add your subtitle text here
               style: TextStyle(
                 fontSize: 14,
                 color: Theme.of(context).colorScheme.secondary.withOpacity(0.7),
@@ -160,7 +181,8 @@ class _DisplayPageState extends State<DisplayPage> {
         height: 60, // Adjust the height as desired
         child: NavigationBar(
           labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-          backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.1),
+          backgroundColor:
+              Theme.of(context).colorScheme.primary.withOpacity(0.1),
           selectedIndex: _selectedIndex,
           onDestinationSelected: _onItemTapped,
           destinations: <Widget>[
@@ -178,14 +200,16 @@ class _DisplayPageState extends State<DisplayPage> {
             ),
             NavigationDestination(
               selectedIcon: CircleAvatar(
-                backgroundImage: NetworkImage('https://example.com/profile_picture.png'), // Replace with actual profile picture URL
+                backgroundImage: NetworkImage(
+                    profilePicture), // Replace with actual profile picture URL
                 radius: 16, // Adjust the radius to make the image smaller
               ),
               icon: CircleAvatar(
-                backgroundImage: NetworkImage('https://example.com/profile_picture.png'), // Replace with actual profile picture URL
+                backgroundImage: NetworkImage(
+                    profilePicture), // Replace with actual profile picture URL
                 radius: 16, // Adjust the radius to make the image smaller
               ),
-              label: 'Profile',
+              label: userHandle,
             ),
           ].where((destination) => destination != null).toList(),
         ),
